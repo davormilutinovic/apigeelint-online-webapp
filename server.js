6ec30e725f9520f502b18e3d714d4aaf77b8dbaf
Davor Milutinovic <63594043+davormilutinovic@users.noreply.github.com>
2023-10-20 09:34:25 +0200

    added sharedflows

--- server.js
+++ server.js
@@ -16,6 +16,23 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+function cleanBundleArtifacts(filePath, extractPath) {
+
+    // Delete the extracted folder after processing the bundle
+    fs.rm(extractPath, { recursive: true, force: true }, (err) => {
+        if (err) {
+            console.error(`Error deleting extracted folder: ${err}`);
+        }
+    });
+
+    // Delete the uploaded zip file
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(`Error deleting zip file: ${err}`);
+        }
+    });
+}
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -28,7 +45,18 @@ app.post('/upload', upload.single('file'), (req, res) => {
     const extractPath = path.join(__dirname, 'extracted-' + req.file.originalname);
     zip.extractAllTo(extractPath, true);
 
-    const command = `apigeelint -s "${extractPath}"/apiproxy -f table.js `;
+    // Find bundle type (apiproxy or sharedflow)
+    var bundleType;
+    if (fs.existsSync(extractPath + '/apiproxy'))
+        bundleType = 'apiproxy';
+    else if (fs.existsSync(extractPath + '/sharedflows'))
+        bundleType = 'sharedflows';
+    else {
+        cleanBundleArtifacts(filePath, extractPath);
+        return res.send('Bundle must have either apiproxy or sharedflow directory');
+    }
+
+    const command = `apigeelint -s ${extractPath}/${bundleType} -f table.js `;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
@@ -41,20 +69,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
             return res.send(stderr);
         }
 
-        // Delete the extracted folder after processing the bundle
-        fs.rm(extractPath, { recursive: true, force: true }, (err) => {
-            if (err) {
-                console.error(`Error deleting extracted folder: ${err}`);
-            }
-        });
-
-        // Delete the uploaded zip file
-        fs.unlink(filePath, (err) => {
-            if (err) {
-                console.error(`Error deleting zip file: ${err}`);
-            }
-        });
-
+        cleanBundleArtifacts(filePath, extractPath);
         res.send(stdout);
 
     });
